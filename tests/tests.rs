34f020f2716be33a90a0ff9de9bff224180dddf3
Wellington Barbosa <wmb.software.consulting@gmail.com>
2025-06-08 13:32:09 +0100

    Add refund tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -15,7 +15,7 @@ use spl_associated_token_account_client::address::get_associated_token_address;
 use spl_token::{ state::{ Account as SPLTokenAccount, Mint as SPLMint, AccountState } };
 
 use pinocchio_token::state::{ TokenAccount, Mint };
-use pinocchio_escrow::{ to_bytes, Escrow, Make, MakeInstructionData, Take, ID };
+use pinocchio_escrow::{ to_bytes, Escrow, Make, MakeInstructionData, Refund, Take, ID };
 
 pub const PROGRAM: Pubkey = Pubkey::new_from_array(ID);
 
@@ -440,6 +440,166 @@ fn test_take() {
             Check::account(&taker_ata_a).data(serialized_expected_taker_ata_a.as_slice()).build(),
             Check::account(&taker_ata_b).data(serialized_expected_taker_ata_b.as_slice()).build(),
             Check::account(&maker_ata_b).data(serialized_expected_maker_ata_b.as_slice()).build(),
+        ]
+    );
+}
+
+#[test]
+fn test_refund() {
+    let mollusk = mollusk();
+    let (system_program, system_account) = mollusk_svm::program::keyed_account_for_system_program();
+
+    let (token_program, token_program_account) = mollusk_svm_programs_token::token::keyed_account();
+
+    let (ata_program, ata_program_account) =
+        mollusk_svm_programs_token::associated_token::keyed_account();
+
+    let maker = Pubkey::new_from_array([0x01; 32]);
+    let maker_account = Account::new(10 * LAMPORTS_PER_SOL, 0, &system_program);
+
+    let mint_a = Pubkey::new_from_array([0x02; 32]);
+    let mut mint_a_account = Account::new(
+        mollusk.sysvars.rent.minimum_balance(Mint::LEN),
+        Mint::LEN,
+        &token_program
+    );
+    SPLMint::pack(
+        SPLMint {
+            mint_authority: COption::None,
+            supply: 100_000_000,
+            decimals: 6,
+            is_initialized: true,
+            freeze_authority: COption::None,
+        },
+        mint_a_account.data_as_mut_slice()
+    ).unwrap();
+
+    let mint_b = Pubkey::new_from_array([0x03; 32]);
+    let mut mint_b_account = Account::new(
+        mollusk.sysvars.rent.minimum_balance(Mint::LEN),
+        Mint::LEN,
+        &token_program
+    );
+    SPLMint::pack(
+        SPLMint {
+            mint_authority: COption::None,
+            supply: 100_000_000,
+            decimals: 6,
+            is_initialized: true,
+            freeze_authority: COption::None,
+        },
+        mint_b_account.data_as_mut_slice()
+    ).unwrap();
+
+    let seed = 123456789u64;
+    let (escrow, escrow_bump) = solana_sdk::pubkey::Pubkey::find_program_address(
+        &[Escrow::SEED.as_bytes(), &maker.to_bytes(), seed.to_le_bytes().as_ref()],
+        &PROGRAM
+    );
+    let escrow_account = Account {
+        lamports: 10 * LAMPORTS_PER_SOL,
+        data: (
+            unsafe {
+                to_bytes::<Escrow>(
+                    &(Escrow {
+                        is_initialized: true,
+                        seed,
+                        maker: maker.to_bytes(),
+                        mint_a: mint_a.to_bytes(),
+                        mint_b: mint_b.to_bytes(),
+                        receive: 10 * LAMPORTS_PER_SOL,
+                        bump: escrow_bump,
+                    })
+                )
+            }
+        ).to_vec(),
+        owner: PROGRAM,
+        executable: false,
+        rent_epoch: 0,
+    };
+
+    let mut vault_account = Account::new(
+        mollusk.sysvars.rent.minimum_balance(TokenAccount::LEN),
+        TokenAccount::LEN,
+        &token_program
+    );
+    let vault = get_associated_token_address(&escrow, &mint_a);
+
+    SPLTokenAccount::pack(
+        SPLTokenAccount {
+            mint: mint_a,
+            owner: escrow,
+            amount: 15 * LAMPORTS_PER_SOL,
+            delegate: COption::None,
+            state: AccountState::Initialized,
+            is_native: COption::None,
+            delegated_amount: 0,
+            close_authority: COption::None,
+        },
+        vault_account.data_as_mut_slice()
+    ).unwrap();
+
+    let maker_ata_a_account = Account::new(0, 0, &system_program);
+    let maker_ata_a = get_associated_token_address(&maker, &mint_a);
+
+    let instruction = Instruction::new_with_bytes(
+        PROGRAM,
+        &vec![*Refund::DISCRIMINATOR],
+        vec![
+            AccountMeta::new(maker, true),
+            AccountMeta::new(escrow, false),
+            AccountMeta::new_readonly(mint_a, false),
+            AccountMeta::new(vault, false),
+            AccountMeta::new(maker_ata_a, false),
+            AccountMeta::new_readonly(ata_program, false),
+            AccountMeta::new_readonly(token_program, false),
+            AccountMeta::new_readonly(system_program, false)
+        ]
+    );
+
+    let expected_escrow = Escrow {
+        is_initialized: true,
+        seed,
+        maker: maker.to_bytes(),
+        mint_a: mint_a.to_bytes(),
+        mint_b: mint_b.to_bytes(),
+        receive: 10 * LAMPORTS_PER_SOL,
+        bump: escrow_bump,
+    };
+    let serialized_expected_escrow = unsafe { to_bytes::<Escrow>(&expected_escrow) };
+
+    let expected_maker_ata_a = SPLTokenAccount {
+        mint: mint_a,
+        owner: maker,
+        amount: 15 * LAMPORTS_PER_SOL,
+        delegate: COption::None,
+        state: AccountState::Initialized,
+        is_native: COption::None,
+        delegated_amount: 0,
+        close_authority: COption::None,
+    };
+
+    let serialized_expected_maker_ata_a = {
+        data_to_bytes::<SPLTokenAccount>(expected_maker_ata_a)
+    };
+
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &vec![
+            (maker, maker_account),
+            (escrow, escrow_account),
+            (mint_a, mint_a_account),
+            (vault, vault_account),
+            (maker_ata_a, maker_ata_a_account),
+            (ata_program, ata_program_account),
+            (token_program, token_program_account),
+            (system_program, system_account)
+        ],
+        &[
+            Check::success(),
+            Check::account(&escrow).data(serialized_expected_escrow).build(),
+            Check::account(&vault).data(&[]).build(),
+            Check::account(&maker_ata_a).data(serialized_expected_maker_ata_a.as_slice()).build(),
         ]
     );
 }
